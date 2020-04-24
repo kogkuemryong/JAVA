e42a2311aba51b4c47aed86bb4d73beed956d29e
LG <LG@DESKTOP-3Q93DO8>
2020-04-24 23:31:04 +0900

    commit

---
+++ src/객체지향언어/AppleMain.java
@@ -0,0 +1,68 @@
+package 객체지향언어;
+
+class Mart{                // 참조자료형이라는 의미로 첫글자는 대문자로 한다. 	
+	// field (속성) 
+	int benefit; // 이윤
+	int numOfApple ; // 남은 사과 갯수
+	
+	
+	 Mart(int apple){
+		 //System.out.println("실행 ...");
+		 numOfApple =  apple;
+		 
+		 
+	 }
+	
+
+   //method(사과를 판매 - 행위/기능) 
+	public void sellApple(int money) {
+		
+		int num = 0;	
+		num = money / 1000;
+		numOfApple -= num;       // 사과의 재고가 빼지면서 누적된다. 
+		benefit += money; // 복합대입연산자로 이윤을 누적시킨다. 		
+    }
+	// 결산
+	public void displayMart() {
+		System.out.println(" 마트 이윤 : " + benefit);
+		System.out.println(" 남은 사과 갯수 : " + numOfApple);
+
+	}		
+}
+
+public class AppleMain {
+
+	public static void main(String[] args) {
+		
+		Mart e_mart;              // 생성자 : class 의 이름과 같아야 한다. 반환형이 없다. java에 의해서 memory 생성시 딱 1번만 수행된다. 
+		e_mart = new Mart(100);   //  1) 메모리를 할당해주고, Mart의 주소값을 준다.
+		                          //  2) 위의 Mart() 를 호출해준다. : 초기화를 해준다. 
+		e_mart.sellApple(98000);  //  필드의 값을 메모리에 할당해서 그 값으 주소값을 주고 초기화 하는데 활용하게 해준다. 
+	 	e_mart.displayMart();
+		
+		e_mart.sellApple(2000);
+		e_mart.displayMart();
+		
+		
+	    Mart homePlus;
+	    homePlus = new Mart(100);
+	    
+	    homePlus.sellApple(100000);
+	    homePlus.displayMart();
+	    
+	    
+	    Mart department;
+	    department = new Mart(100);
+	    
+	    department.sellApple(100000);
+	    department.displayMart();
+	    
+		// 자료형만 잘 지어주면 결과값은 언제든지 얻어낼 수 있다. 
+	    // 객체의 이름만 알고 있다면 언제든 수행할 수 있다. 
+	    // 자료형을 만들어두면 새로운 객체가 생길 때 마다 그 값
+
+		
+		
+	}
+
+}
---
+++ src/객체지향언어/OOPBasic.java
@@ -0,0 +1,134 @@
+package 객체지향언어;
+
+class Employee{                                
+                               // 사원이라는 대상의 속성으로 만들어낸 틀이다. 
+// field / 멤버변수                          // class : 틀을 만들어주는 명령어
+int empno;                     // 선언만 되어 있을 뿐이지  
+String ename;                  // main 안에 들어있지 않기 때문에 결과가 수행되지 않는다.
+String job;                    // Employee 이름의 자료형으로 내가 저장해준것이다. 
+int mgr;
+int sal; 
+int deptno;	  
+
+
+//멤버 메서드 (인스턴스 메서드)-- 기능 추가
+public void displayEmployee() {                //카맬 방식으로 두단어를 합친 이름만들기
+System.out.println("사원번호 : " + empno);
+System.out.println("사원이름 : " + ename);
+System.out.println("직책 : " + job);
+System.out.println("사수번호 : " + mgr);
+System.out.println("급여 : " + sal);
+System.out.println("부서번호 : " + deptno);
+
+// king.displayEmployee(); // main 안에서 입력 했을 떄 밑의 값이 출력된다. 
+/*
+* 사원번호 : 1000 
+* 사원이름 : 사장님 
+* 직책 : 관리 
+* 사수번호 : 0
+* 급여 : 5000 
+* 부서번호 : 10
+*/
+
+
+}
+
+}
+
+class sale{
+
+String name;
+int tax;
+int price;
+int sum;
+
+public void displaysale()	{
+System.out.println("과일 이름 :" + name);
+System.out.println("세금 :" + tax);
+System.out.println("금액 :" + price);
+System.out.println("수익합계 :" + sum);
+
+
+}
+
+
+
+}
+
+
+
+public class OOPBasic {
+
+	public static void main(String[] args) {
+		
+
+		Employee king ; // 참조변수(주소값) 내가 만든 자료형으로 변수 선언 / Employee 6개의 데이터를 가지고 있을 수 있다.
+		king = new Employee();        // 참조 자료형  , 인스턴스화 -> 인스턴스(메모리할당)  
+		// 주소값 : 할당 받은 memory의 시작 주소( 1개의 값만 저장할 수 있다.) 
+		// 주소값을 통해서 내가 지정한 자료형으로 갈 수 있다. 
+		// king 의 값을 주소값을 불러주는 참조변수라고 불러준다. (주소값을 가지고 있다. = 객체를 가지고 있고, 인스턴스화 시킨다.)
+		// 참조변수는 별도로 4byte 할당 받는다. 
+		// 이제부터 각자 관리 되는 것이 아니라 king하나로 관리하겠다.
+		
+		king.empno = 1000; // 인스턴트 변수 적용
+		king.ename = "사장님";
+		king.job = "관리";
+		king.mgr =0;
+		king.sal = 5000;
+		king.deptno = 10;
+				
+		king.displayEmployee();
+		
+		/*
+		 * 사원번호 : 1000 
+		 * 사원이름 : 사장님 
+		 * 직책 : 관리 
+		 * 사수번호 : 0
+		 * 급여 : 5000 
+		 * 부서번호 : 10
+		 */
+	
+		// 새로운 사람을 넣을 때! 
+		Employee scott ;
+		scott = new Employee();  // 1) 할당을 받는다. 
+		
+		scott.empno = 1001; // 이제부터 각자 관리 되는 것이 아니라 king하나로 관리하겠다.
+		scott.ename = "scott";
+		scott.job = "salesman";
+		scott.mgr =1000;
+		scott.sal = 2500;
+		scott.deptno = 2;
+		
+	    scott.displayEmployee();
+	   
+	    sale apple ;
+		apple = new sale();
+		
+		apple.name = "apple";
+		apple.tax = 100;
+		apple.price =  1000;
+		apple.sum = (apple.price -apple.tax);
+		
+		apple.displaysale();
+	    
+	    /*
+		 * 사원번호 : 1001
+		 * 사원이름 : scott
+		 * 직책 : salesman
+		 * 사수번호 : 1000
+		 * 급여 : 2500
+		 * 부서번호 : 2
+		 */
+	    
+		// (new : 만들어둔 틀의 저장 공간(memory)을 할당 받기 위한 명령어이다.) //
+        // new Employee(); 형식은 method 수행하는 형식과 같이 사용한다. 
+        // 요청은 내가 했지만 memory 영역은 java가 할당해준다. 
+        // 실체화 한 대상  : 범용적인 객체 = 할당 되어 있는 메모리에 있는 정보 / 객체 할당하기 시작하는 이름 
+		// 인스턴스 : 할당 되어 있는 메모리 공간
+	    // 추상적인 개념인 클래스에서 실제 객체를 생성하는 것을 인스턴스화(instantiation)라고 한다.                                            	 
+		
+		
+
+	}
+
+}
